4fa3aa411284c3c8a30ab6a8efc33ed5c080e066
zboukhris <boukhriszakaria@gmail.com>
2018-05-08 17:19:46 +0100

    Factories folder

---
+++ DesignPatterns/Builder/CodeBuilder/CodeBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder.CodeBuilder
+{
+    public class CodeBuilder
+    {
+        private string _className { get; set; }
+        private List<Tuple<string, string>> _propertyList { get; set; }
+        public CodeBuilder(string className)
+        {
+            _className = className;
+            _propertyList = new List<Tuple<string, string>>();
+        }
+
+        public CodeBuilder AddField(string type, string propertyName)
+        {
+            _propertyList.Add(Tuple.Create(type, propertyName));
+            return this;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder 
+        }
+    }
+}
---
+++ DesignPatterns/Factories/FactoryStartingPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Factories
+{
+    class FactoryStartingPoint : IRun
+    {
+        public void DoRun()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
