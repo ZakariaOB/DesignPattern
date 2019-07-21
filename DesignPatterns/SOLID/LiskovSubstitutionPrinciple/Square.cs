c9620a7f1c80c3a701cd9a8fac0e96b0d7ff4f73
Boukhris Zakaria <boukhriszakaria@gmail.com>
2019-07-21 16:29:35 +0100

    Add repo 2

--- DesignPatterns/SOLID/LiskovSubstitutionPrinciple/Square.cs
+++ DesignPatterns/SOLID/LiskovSubstitutionPrinciple/Square.cs
@@ -8,9 +8,20 @@ namespace DesignPatterns.SOLID.LiskovSubstitutionPrinciple
 {
     public class Square : Rectangle
     {
+        public string AddRepo2()
+        {
+            return "repo 2";
+        }
+
         public override void Show()
         {
             Console.WriteLine("Show square");
+        }
+
+
+        public string AddRepo2_2()
+        {
+            return "repo 22";
         }
     }
 }
