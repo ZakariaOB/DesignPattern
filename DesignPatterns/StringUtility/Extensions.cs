ff55ea776dc156b2ea265237744e403bd4de9f4f
Boukhris Zakaria <boukhriszakaria@gmail.com>
2019-07-21 15:03:36 +0100

    Add extensions 1 and extension 2

--- DesignPatterns/StringUtility/Extensions.cs
+++ DesignPatterns/StringUtility/Extensions.cs
@@ -12,6 +12,11 @@
             return text.ToUpper();
         }
 
+        public static string AddExtension2()
+        {
+            return "Extension 2, to know as important as it is";
+        }
+
         public static string[] ToArray(this string text, char separator)
         {
             if (string.IsNullOrEmpty(text))
@@ -21,6 +26,11 @@
 
             var tab = text.Split(new char[] { '-' });
             return tab;
+        }
+
+        public static string AddExtension()
+        {
+            return "Extension 1";
         }
     }
 }
